a0ec32d4608d7c90c1c1632e72a3e135ba19d202
ambujtiwari3492 <124238189+ambujtiwari3492@users.noreply.github.com>
2023-08-07 17:24:12 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-8-getting-started-Ambuj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-8-getting-started-i3zjv7)
---
+++ src/app/children/children.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-children3',
+  templateUrl: './children.component.html',
+  styleUrls: ['./children.component.css'],
+})
+export class ChildrenComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
